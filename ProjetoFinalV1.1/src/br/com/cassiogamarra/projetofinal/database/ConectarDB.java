0dec6bd4c2e0239bc4c37ac005b7f0977466a1d2
Cassio Gamarra <cassio1515@hotmail.com>
2019-05-23 23:10:57 -0300

    DB

--- ProjetoFinalV1.1/src/br/com/cassiogamarra/projetofinal/database/ConectarDB.java
+++ ProjetoFinalV1.1/src/br/com/cassiogamarra/projetofinal/database/ConectarDB.java
@@ -22,32 +22,3 @@ public class ConectarDB{
 	return conectar;
     }
 }
-
-/* *COMANDO SQL PARA CRIAR O BANCO*
-CREATE DATABASE bancocatracas;
-USE bancocatracas;
-CREATE TABLE usuario(
-codigo BIGINT(10) AUTO_INCREMENT,
-nome VARCHAR(200),
-cpf VARCHAR(20),
-telefone VARCHAR(20),
-dependentes INT(10),
-turma VARCHAR(50),
-cargo VARCHAR(50),
-observacoes VARCHAR(200),
-categoria VARCHAR(30),
-situacao TINYINT(1),
-PRIMARY KEY (codigo)
-);
-
-ALTER TABLE usuario AUTO_INCREMENT = 12000;
-*/
-
-/*
-Banco externo:
-phpmyadmin.umbler.com -> mysql465.umbler.com
-
-mysql465.umbler.com:41890
-cassiogamarra
-O+f5pl*-O3[Z
-*/
