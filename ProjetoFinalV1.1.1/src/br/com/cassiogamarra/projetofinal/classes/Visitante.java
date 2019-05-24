764a79cfeb0851e26c2237bfa872522bac527179
Cassio Gamarra <cassio1515@hotmail.com>
2019-05-23 23:32:56 -0300

    ProjetoFinal

---
+++ ProjetoFinalV1.1.1/src/br/com/cassiogamarra/projetofinal/classes/Visitante.java
@@ -0,0 +1,14 @@
+package br.com.cassiogamarra.projetofinal.classes;
+
+public class Visitante extends Pessoa{
+    private String observacoes;
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+    
+}
