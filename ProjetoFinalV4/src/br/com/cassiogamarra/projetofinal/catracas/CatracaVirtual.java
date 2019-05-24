ccf06b34fb765d5ccf81180b62d00a75cc7d1458
Cássio Gamarra <cassio1515@hotmail.com>
2019-05-24 20:51:18 -0300

    Java

--- ProjetoFinalV4/src/br/com/cassiogamarra/projetofinal/catracas/CatracaVirtual.java
+++ ProjetoFinalV4/src/br/com/cassiogamarra/projetofinal/catracas/CatracaVirtual.java
@@ -28,31 +28,41 @@ public class CatracaVirtual{
             }
              else{
                 //Procura o nome da pessoa
-                sql = "SELECT * FROM usuario WHERE situacao = 1 AND codigo = "+codigo;
+                sql = "SELECT * FROM usuario WHERE codigo = "+codigo;
                 String nome = "";
+                int situacao = 0;
                 try{
                     stmt = conectar.prepareStatement(sql);
                     consulta = stmt.executeQuery(sql);
                     while(consulta.next()){
                         nome = consulta.getString("nome");
+                        situacao = consulta.getInt("situacao");
+                    }
+                    if(situacao == 0){
+                        JOptionPane.showMessageDialog(null, "PESSOA INATIVA!!!");
+                    }
+                    else{
+                        //Faz a entrada da pessoa
+                        sql = "INSERT INTO catraca(codigo, situacao)VALUES(?,?)";
+                        try{
+                            stmt = conectar.prepareStatement(sql);
+                            stmt.setLong(1, codigo);
+                            stmt.setInt(2, 1);
+                            stmt.execute();
+                            JOptionPane.showMessageDialog(null, "BEM VINDO: "+nome);
+                        }
+
+                        catch(SQLException e){
+                            throw new RuntimeException(e);
+                        }
                     }
                 }
                 catch(SQLException e){
                    throw new RuntimeException(e);
                 }
-                //Faz a entrada da pessoa
-                sql = "INSERT INTO catraca(codigo, situacao)VALUES(?,?)";
-                try{
-                    stmt = conectar.prepareStatement(sql);
-                    stmt.setLong(1, codigo);
-                    stmt.setInt(2, 1);
-                    stmt.execute();
-                    JOptionPane.showMessageDialog(null, "BEM VINDO: "+nome);
-                }
-
-                catch(SQLException e){
-                    throw new RuntimeException(e);
-                }
+                
+                
+                
             }
         }
         catch(SQLException e){
@@ -62,7 +72,7 @@ public class CatracaVirtual{
        
         
     }
-    public static void saida(long codigo) {
+    public static void saida(FrameCatracas frame) {
         
     }
 }
