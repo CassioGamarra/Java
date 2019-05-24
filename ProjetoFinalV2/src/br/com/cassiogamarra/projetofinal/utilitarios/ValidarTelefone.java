fc7b8f1fb67471f396815ccb34267f8315216947
Cassio Gamarra <cassio1515@hotmail.com>
2019-05-23 23:36:35 -0300

    Projeto Final V2

---
+++ ProjetoFinalV2/src/br/com/cassiogamarra/projetofinal/utilitarios/ValidarTelefone.java
@@ -0,0 +1,16 @@
+package br.com.cassiogamarra.projetofinal.utilitarios;
+
+public class ValidarTelefone {
+    public ValidarTelefone(){
+        
+    }
+    
+    public static boolean validarTelefone(String telefone){
+        
+        if(telefone.matches("(10)|([1-9][1-9])[9][2-9][0-9]{3}[0-9]{4}")||
+                telefone.matches("(10)|([1-9][1-9])[2-9][0-9]{3}[0-9]{4}")){
+            return true;
+        }
+        return false;
+    }
+}
