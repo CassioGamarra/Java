f8fc201d2bffcf46e5b86e5fdcc345502c633aa9
CassioGamarra <cassio1515@hotmail.com>
2019-08-15 09:20:25 -0300

    Projeto 1

---
+++ Linguagens II/ModuloRH/src/modulorh/Cadastro.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modulorh;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author cassio
+ */
+public class Cadastro {
+
+    public Cadastro() {
+    }
+    
+    public void cadastrar(JFrame frame){
+        JOptionPane.showMessageDialog(null, "SUCESSO!");
+    }
+}
---
+++ Linguagens II/ModuloRH/src/modulorh/ModuloRH.java
@@ -0,0 +1,16 @@
+package modulorh;
+
+/**
+ * Módulo RH - Disciplina de Linguagens II
+ * @author cassio gamarra
+ */
+public class ModuloRH {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new FormCadastro().setVisible(true);
+    }
+    
+}
